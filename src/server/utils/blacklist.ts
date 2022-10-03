0b95e1e4122828562c00ec79a97a6a42d283201c
Burhan Syed <burhanudinsyed@gmail.com>
2022-10-03 19:20:26 -0400

    stream blacklist

--- src/server/utils/blacklist.ts
+++ src/server/utils/blacklist.ts
@@ -1,32 +1,21 @@
-import fs, { createWriteStream } from "fs";
-import fsPromises from "fs/promises";
-import { pipeline } from "stream";
-import { promisify } from "util";
+
 
 const BLACKLIST_SOURCE = import.meta.env.BLACKLIST_SOURCE;
-const BLACKLIST_PATH = "./blacklist.txt";
+//const BLACKLIST_PATH = "./blacklist.txt";
 
-const downloadFile = async (url: string, path: string) => {
-  const streamPipeline = promisify(pipeline);
+const fetchFile = async (url: string) => {
   const res = await fetch(url);
   if (!res.ok || !res.body) {
     throw new Error("unable to fetch file");
   }
-  await streamPipeline(res.body as any, createWriteStream(path));
+  return await res.text()
 };
 
-const getBlacklist = async () => {
-  if (fs.existsSync(BLACKLIST_PATH)) {
-  } else {
-    await downloadFile(BLACKLIST_SOURCE, BLACKLIST_PATH);
-  }
-};
 
 export const checkInBlacklist = async (url: string) => {
   const urlToCheck = url.replace("https://","").replace("https://",""); 
   try {
-    await getBlacklist();
-    const contents = await fsPromises.readFile(BLACKLIST_PATH, "utf-8");
+    const contents = await fetchFile(BLACKLIST_SOURCE);
     const contains = contents.includes(urlToCheck);
     return contains;
   } catch (err) {
