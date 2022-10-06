ee33d884eb4e5efe1ee0a03cf56b905b43177072
Burhan Syed <burhanudinsyed@gmail.com>
2022-10-05 22:16:55 -0400

    fix firefox nav overflow

--- src/components/ui/ExploreNavBar.tsx
+++ src/components/ui/ExploreNavBar.tsx
@@ -78,6 +78,7 @@ export default function ExploreNavBar({
           }}
           htmlFor="my-drawer-4"
           className="btn btn-ghost drawer-button swap swap-rotate w-16"
+          aria-label="side navigation"
         >
           <input id={"nav-swap-checkbox2"} ref={inputRef} type="checkbox" />
           <svg
@@ -95,10 +96,10 @@ export default function ExploreNavBar({
             />
           </svg>
           <svg
-            className="swap-on fill-current"
+            className="swap-on fill-current mx-auto"
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="28"
+            height="28"
             viewBox="0 0 512 512"
           >
             <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
